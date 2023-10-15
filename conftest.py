8873bc880cc7a6deab4ae56129bebb319b2966c7
Yura <gyrra777@gmail.com>
2023-10-15 14:18:07 +0300

    добавляю логи

--- conftest.py
+++ conftest.py
@@ -41,49 +41,48 @@ def browser(request):
     executor = request.config.getoption("--executor")
 
     class WebdriverListener(AbstractEventListener):
-        # logger = logging.getLogger(request.node.name)
-        # logger.setLevel(logging.INFO)
-        # ch = logging.FileHandler(filename=f"./logs/{request.node.name}.log")
-        # ch.setFormatter(logging.Formatter('%(name)s:%(levelname)s %(message)s'))
-        # ch.setLevel(log_map[log_level])
-        # logger.addHandler(ch)
-        pass
+        logger = logging.getLogger(request.node.name)
+        logger.setLevel(logging.INFO)
+        ch = logging.FileHandler(filename=f"./logs/{request.node.name}.log")
+        ch.setFormatter(logging.Formatter('%(name)s'))
+        ch.setLevel(log_map[log_level])
+        logger.addHandler(ch)
 
-        # def before_navigate_to(self, url, driver):
-        #     self.logger.info(f"I'm navigating to {url} and {driver.title}")
-        #
-        # def after_navigate_to(self, url, driver):
-        #     self.logger.info(f"I'm on {url}")
-        #
-        # def before_navigate_back(self, driver):
-        #     self.logger.info(f"I'm navigating back")
-        #
-        # def after_navigate_back(self, driver):
-        #     self.logger.info(f"I'm back!")
-        #
-        # def before_find(self, by, value, driver):
-        #     self.logger.info(f"I'm looking for '{value}' with '{by}'")
-        #
-        # def after_find(self, by, value, driver):
-        #     self.logger.info(f"I've found '{value}' with '{by}'")
-        #
-        # def before_click(self, element, driver):
-        #     self.logger.info(f"I'm clicking {element}")
-        #
-        # def after_click(self, element, driver):
-        #     self.logger.info(f"I've clicked {element}")
-        #
-        # def before_execute_script(self, script, driver):
-        #     self.logger.info(f"I'm executing '{script}'")
-        #
-        # def after_execute_script(self, script, driver):
-        #     self.logger.info(f"I've executed '{script}'")
-        #
-        # def before_quit(self, driver):
-        #     self.logger.info(f"I'm getting ready to terminate {driver}")
-        #
-        # def after_quit(self, driver):
-        #     self.logger.info(f"Driver Quit")
+        def before_navigate_to(self, url, driver):
+            self.logger.info(f"I'm navigating to {url} and {driver.title}")
+
+        def after_navigate_to(self, url, driver):
+            self.logger.info(f"I'm on {url}")
+
+        def before_navigate_back(self, driver):
+            self.logger.info(f"I'm navigating back")
+
+        def after_navigate_back(self, driver):
+            self.logger.info(f"I'm back!")
+
+        def before_find(self, by, value, driver):
+            self.logger.info(f"I'm looking for '{value}' with '{by}'")
+
+        def after_find(self, by, value, driver):
+            self.logger.info(f"I've found '{value}' with '{by}'")
+
+        def before_click(self, element, driver):
+            self.logger.info(f"I'm clicking {element}")
+
+        def after_click(self, element, driver):
+            self.logger.info(f"I've clicked {element}")
+
+        def before_execute_script(self, script, driver):
+            self.logger.info(f"I'm executing '{script}'")
+
+        def after_execute_script(self, script, driver):
+            self.logger.info(f"I've executed '{script}'")
+
+        def before_quit(self, driver):
+            self.logger.info(f"I'm getting ready to terminate {driver}")
+
+        def after_quit(self, driver):
+            self.logger.info(f"Driver Quit")
 
     if browser_name == "firefox":
         options = FirefoxOptions()
